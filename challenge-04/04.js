750d49cddc75b5a68877e237bdac724ae7ff39c0
bruno <brunotheodoro123@gmail.com>
2022-05-09 16:20:10 -0300

    add challenge 03

---
+++ challenge-04/04.js
@@ -0,0 +1,24 @@
+/*
+Declare uma variável chamada `carro`, atribuindo à ela um objeto com as
+seguintes propriedades (os valores devem ser do tipo mostrado abaixo):
+- `marca` - String
+- `modelo` - String
+- `placa` - String
+- `ano` - Number
+- `cor` - String
+- `quantasPortas` - Number
+- `assentos` - Number - cinco por padrão
+- `quantidadePessoas` - Number - zero por padrão
+*/
+
+let carro = {
+  marca: 'fiat',
+  modelo: 'uno',
+  placa: 'kjdw',
+  ano: 2020,
+  cor: azul,
+  quantasPortas: 4,
+  assentos: 5,
+  quantidadePessoas:0,
+
+}
